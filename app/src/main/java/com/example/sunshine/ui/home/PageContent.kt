6e5dc26d26122f7cab54bbd1bd0155f5a207be7d
aamirzainulabadeen <azainulabadeen@gmail.com>
2022-03-20 14:47:22 -0500

    Updated spacing in page content and forecast title.

--- app/src/main/java/com/example/sunshine/ui/home/PageContent.kt
+++ app/src/main/java/com/example/sunshine/ui/home/PageContent.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.sunshine.ui.weather.ForecastTitle
 import com.example.sunshine.ui.weather.FutureWeatherList
 import com.example.sunshine.ui.weather.TodayWeatherHeading
@@ -11,12 +13,31 @@ import com.example.sunshine.ui.weather.TodayWeatherHeading
 @Composable
 fun PageContent() {
     Column(
-        modifier = Modifier.fillMaxWidth(),
+        modifier = Modifier
+            .fillMaxWidth(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         TodayWeatherHeading()
-        ForecastTitle()
-        FutureWeatherList()
+        Column(modifier = Modifier.fillMaxWidth()
+            .padding(horizontal = 4.dp)) {
+            HeadingSpace()
+            ForecastTitle()
+            HeadingSpace()
+            FutureWeatherList()
+        }
     }
+}
+
+@Composable
+fun HeadingSpace() {
+   Spacer(modifier = Modifier
+       .height(16.dp)
+       .fillMaxWidth())
+}
+
+@Preview
+@Composable
+fun PreviewPageContent() {
+    PageContent()
 }
 
--- app/src/main/java/com/example/sunshine/ui/weather/ForecastTitle.kt
+++ app/src/main/java/com/example/sunshine/ui/weather/ForecastTitle.kt
@@ -3,6 +3,7 @@ package com.example.sunshine.ui.weather
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -10,6 +11,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.sunshine.R
 import com.example.sunshine.ui.theme.Blue034
@@ -20,7 +22,8 @@ fun ForecastTitle(titleText: String = "10-Day Forecast") {
         Font(R.font.open_sans_bold),
     )
     Row(horizontalArrangement = Arrangement.Start,
-        modifier = Modifier.fillMaxWidth()) {
+        modifier = Modifier.fillMaxWidth()
+            .padding(start = 8.dp)) {
         Text(
             titleText,
             fontSize = 16.sp,
